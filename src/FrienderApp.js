6ee7268ebd7ef0b82eaf3b3b1f96a8f5b605b62f
Melanie Wong <wong.mela@gmail.com>
2022-05-12 15:25:27 -0700

    got basic skeleton started and first file form

---
+++ src/FrienderApp.js
@@ -0,0 +1,12 @@
+import UpdateProfileForm from "./UpdateProfileForm";
+
+function FrienderApp() {
+  return (
+    <>
+      <h1>Main App</h1>
+      <UpdateProfileForm />
+    </>
+  );
+}
+
+export default FrienderApp;
