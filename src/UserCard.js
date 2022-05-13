6b6152a7338ebea75469262ad84c9e97c0f58f4c
Austin Chao <austinschao@gmail.com>
2022-05-13 12:43:03 -0700

    refactored image url for friends

--- src/UserCard.js
+++ src/UserCard.js
@@ -7,7 +7,7 @@ function UserCard({ user }) {
     <div className="card mb-3" style={{ maxWidth: "540px" }}>
       <div className="row g-0">
         <div className="col-md-4">
-          <img src="https://r25-friender.s3.us-west-1.amazonaws.com/default-pic.png" className="img-fluid rounded-start" alt="..." />
+          <img src={user.image_url} className="img-fluid rounded-start" alt="..." />
         </div>
         <div className="col-md-8">
           <div className="card-body">
