a58d6cbf9d09b931d8291fb48e0811279084415b
Austin Chao <austinschao@gmail.com>
2022-06-05 09:19:31 -0700

    Refactored debounce method

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,24 +2,13 @@ export function randomChoice(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
-// export function debounce_leading(fn, timeout = 300) {
-//   let timer;
-//   return (...args) => {
-//     if (!timer) {
-//       fn.apply(this, args);
-//     }
-//     clearTimeout(timer);
-//     timer = setTimeout(() => {
-//       timer = undefined;
-//     }, timeout);
-//   };
-// }
 
-export function debounce(fn, timeout = 5000) {
-  let timer;
-  return function (...args) {
-    clearTimeout(timer);
-    timer = setTimeout(() => fn.apply(this, args), timeout);
-
+export function debounce(cb, delay = 500) {
+  let timeout;
+  return (...args) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      cb(...args);
+    }, delay);
   };
 }
