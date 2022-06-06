d9618d7dec243bf1260a0f1b141c5cb3edfa1069
Austin Chao <austinschao@gmail.com>
2022-06-05 18:42:49 -0700

    Refactored styling of friender

--- src/UserCard.js
+++ src/UserCard.js
@@ -21,17 +21,17 @@ function UserCard({ user, handleMatch, handleReject }) {
   // Need to show a link to display list of messages between each other
   // Include a button to add a message that will send a post request to API
   return (
-    <div className="card mb-3" style={{ maxWidth: "540px" }}>
-      <div className="row g-0">
-        <div className="col-md-4">
+    <div className="card mb-3 position-absolute top-50 start-50 translate-middle" style={{ maxWidth: "540px" }}>
+      < div className="row g-0" >
+        <div className="col-md-4 py-4">
           <img src={user.image_url} className="img-fluid rounded-start" alt="..." />
         </div>
         <div className="col-md-8">
           <div className="card-body">
             <h5 className="card-title">{user.username}</h5>
-            <ul>
-              <li className="card-text">Interests: {user.interests.replace(/[\[\]']+/g, '')}</li>
-              <li className="card-text">Hobbies: {user.hobbies.replace(/[\[\]']+/g, '')}</li>
+            <ul className="list-unstyled">
+              <li className="card-text"><b>Interests</b>: {user.interests.replace(/[\[\]']+/g, '')}</li>
+              <li className="card-text"><b>Hobbies</b>: {user.hobbies.replace(/[\[\]']+/g, '')}</li>
             </ul>
             <div className="d-flex justify-content-center">
               <button className="btn btn-success btn-sm mx-1" onClick={() => handleMatch(currentUser, user)}>Match</button>
@@ -39,8 +39,8 @@ function UserCard({ user, handleMatch, handleReject }) {
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      </div >
+    </div >
   );
 };
 
